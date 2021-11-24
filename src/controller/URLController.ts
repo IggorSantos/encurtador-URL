0840321508b6be619d93409b5f1a306ef82ae478
Igor Santos <igor.sagitarius@gmail.com>
2021-11-24 09:22:41 -0300

    Conexão com o mongodb

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,22 +1,31 @@
 import {Request,Response} from 'express'
 import shortId from 'shortid'
-import{config} from '../config/Constants'
+import {config} from '../config/Constants'
+import {URLModel} from '../database/model/URL'
 
 export class URLController{
   public async shorten(req:Request, res:Response): Promise<void>{
     const {originURL} = req.body
+    const url = await URLModel.findOne({ originURL })
+		if (url) {
+			response.json(url)
+			return
+		}
     const hash = shortId.generate()
     const shortURL = `${config.API_URL}/${hash}`
-    res.json({ originURL, hash, shortURL})
+    const newURL = await URLModel.create({ hash, shortURL, originURL })
+		response.json(newURL)
 
   }
   public async redirect(req:Request, res:Response): Promise<void>{
     const { hash } = req.params
-    const url = {
-      originURL: "https://www.youtube.com/",
-      hash: "-mNtjO_Ka",
-      shortURL: "http://localhost:5000/-mNtjO_Ka"
+    const url = await URLModel.findOne({ hash })
+    
+    if (url) {
+      response.redirect(url.originURL)
+      return
     }
-    res.redirect(url.originURL)
+
+    response.status(400).json({ error: 'URL not found' })
   }
 }
